994e828cc2e8ad1469633bc1eeea3fd9b2a530aa
Nathan <zhangruonan@hungrystudio.com>
2026-06-08 13:34:28 +0800

    chore(ws): add public re-exports

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,2 +1,12 @@
+// src/ws/mod.rs
 pub mod proto;
 pub mod business;
+
+pub use proto::client::{WsClient, WsConfig};
+pub use proto::error::WsError;
+pub use proto::headers::MessageType;
+pub use business::registry::HandlerRegistry;
+pub use business::handler::MessageHandler;
+pub use business::types::IncomingMessage;
+pub use business::handlers::event::EventHandler;
+pub use business::handlers::card::{CardActionHandler, CardResponse, Toast};
